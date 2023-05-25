63264d0485ad8f0a4d5133e82c4251e7aa0db082
SuryaaAulia <suryaaulia489@gmail.com>
2023-05-25 14:24:46 +0700

    mengganti LoginView dan update model entity

--- QuiZZila/src/main/java/com/QuiZZila/tubes/Controller/landingController.java
+++ QuiZZila/src/main/java/com/QuiZZila/tubes/Controller/landingController.java
@@ -1,11 +1,15 @@
 package com.QuiZZila.tubes.Controller;
 
+import com.QuiZZila.tubes.Repository.PenggunaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class landingController {
+    @Autowired
+    PenggunaRepository penggunaRepo;
 
     @GetMapping("/join")
     public String landingpage(Model main){
